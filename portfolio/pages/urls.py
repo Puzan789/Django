2ea579276a159a78966e5ba168b50fe40154b9ec
puzan789 <puzan936@gmail.com>
2022-03-27 18:16:11 +0545

    some updates

--- portfolio/pages/urls.py
+++ portfolio/pages/urls.py
@@ -1,13 +1,10 @@
 from django.urls import path
 
-from .views import blogdetailc, blogdetailjava, contact, index,blog,blog_detail
+from .views import  contact, index, blog, blog_detail
 
 urlpatterns = [
     path("", index, name="index"),
     path("contact/", contact, name="contact"),
     path("blog/", blog, name="blog"),
-    path('blog_detail/', blog_detail, name="blog_detail"),
-    path('blogdetailjava/', blogdetailjava, name="blogdetailjava"),
-    path('blogdetailc/', blogdetailc, name="blogdetailc"),
-
+   path('blog_detail/<int:id>', blog_detail, name="blog_detail"),
 ]
